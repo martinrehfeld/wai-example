ae3d2dddbc71fa7d5bba281cdad20b2151c0e092
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-10-10 22:21:06 +0200

    use exec to start runhaskell

--- run-app.hs
+++ run-app.hs
@@ -1,4 +1,5 @@
--- this needs cabal install wai-handler-devel
 import Network.Wai.Handler.DevelServer (runQuit)
 
-main = runQuit 8080 "App" "withApp" (const $ return [])
+main = do
+    putStrLn $ "Warp is starting on http://localhost:8080/ ..."
+    runQuit 8080 "App" "withApp" (const $ return [])
